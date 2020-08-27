fe50b75b26c30969e27d8640acbb79cc16b5dec9
shaivpidadi <shaivpidadi@gmail.com>
2020-08-27 20:19:59 +0530

    Added Header setup

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -12,12 +12,14 @@ import { Switch, Route } from 'react-router-dom';
 
 import HomePage from 'containers/HomePage/Loadable';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
+import Header from '../../components/Header/Header';
 
 import GlobalStyle from '../../global-styles';
 
 export default function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route component={NotFoundPage} />
