ca340a122c2643534348f068bbc35c144ba62294
shaivpidadi <shaivpidadi@gmail.com>
2020-09-07 22:58:24 +0530

    Added All transactions static page

--- app/containers/AllTransctions/index.js
+++ app/containers/AllTransctions/index.js
@@ -5,9 +5,19 @@
  */
 
 import React from 'react';
+import { Card } from '@shopify/polaris';
+import DataTable from '../../components/DataTable/DataTable';
 
 const AllTransctions = () => {
-  return <div> All Transctions</div>;
+  return (
+    <div style={{ marginTop: '40px', width: '100%' }}>
+      <Card sectioned>
+        <Card.Header title="All Transaction" />
+
+        <DataTable />
+      </Card>
+    </div>
+  );
 };
 
 export default AllTransctions;
