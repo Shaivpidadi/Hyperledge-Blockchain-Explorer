e4befa23bc83ae6d853ad1f192dbaac1bd9d8694
shaivpidadi <shaivpidadi@gmail.com>
2020-09-07 23:43:38 +0530

    Added AccordianCard Component

--- app/components/AccordianCard/index.js
+++ app/components/AccordianCard/index.js
@@ -4,14 +4,55 @@
  *
  */
 
-import React from 'react';
-// import PropTypes from 'prop-types';
+import React, { useState, useCallback } from 'react';
 // import styled from 'styled-components';
+import {
+  Card,
+  Stack,
+  Button,
+  Collapsible,
+  TextContainer,
+} from '@shopify/polaris';
 
-function AccordianCard() {
-  return <div />;
-}
+const AccordianCard = ({ title }) => {
+  const [active, setActive] = useState(false);
 
-AccordianCard.propTypes = {};
+  const handleToggle = useCallback(() => setActive(active => !active), []);
+
+  return (
+    <div>
+      <Card sectioned>
+        <Stack vertical>
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'space-between',
+            }}
+          >
+            <Card.Header title={title} />
+            <Card.Header>
+              <Button
+                onClick={handleToggle}
+                ariaExpanded={active}
+                ariaControls="basic-collapsible"
+              >
+                Toggle
+              </Button>
+            </Card.Header>
+          </div>
+          <Collapsible
+            open={active}
+            id="basic-collapsible"
+            transition={{ duration: '150ms', timingFunction: 'ease' }}
+          >
+            <div style={{ padding: '20px' }}>
+              <TextContainer>Coming Soon ...</TextContainer>
+            </div>
+          </Collapsible>
+        </Stack>
+      </Card>
+    </div>
+  );
+};
 
 export default AccordianCard;
--- app/containers/TransactionInfo/index.js
+++ app/containers/TransactionInfo/index.js
@@ -6,6 +6,7 @@
 
 import React from 'react';
 import { Card } from '@shopify/polaris';
+import AccordianCard from '../../components/AccordianCard';
 
 const BlockchainCardItem = ({ label, value }) => (
   <div style={{ lineHeight: '30px', fontSize: '16px' }}>
@@ -34,30 +35,43 @@ const TransactionInfo = () => {
           </div>
 
           <div style={{ padding: '10px 30px', marginTop: '30px' }}>
-            <Card>
-              <div style={{ padding: '20px' }}>
-                <BlockchainCardItem
-                  label="Transaction ID"
-                  value="fbe6ebb45b424"
-                />
-                <BlockchainCardItem label="Validation Code" value="VALID" />
-                <BlockchainCardItem
-                  label="Payload Hash"
-                  value="82a3665a8504337c2af00d"
-                />
-                <BlockchainCardItem label="Creator MSP" value="shaivMSP" />
-                <BlockchainCardItem
-                  label="Endoser"
-                  value="{'sdfasdf', 'asdf'}"
-                />
-                <BlockchainCardItem label="Chaincode" value="SHaIV" />
-                <BlockchainCardItem label="Type" value="ENDORSER_TRANSACTION" />
-                <BlockchainCardItem
-                  label="Time"
-                  value="2020-09-02T13:36:25.469Z"
-                />
-              </div>
-            </Card>
+            <div>
+              <Card>
+                <div style={{ padding: '20px' }}>
+                  <BlockchainCardItem
+                    label="Transaction ID"
+                    value="fbe6ebb45b424"
+                  />
+                  <BlockchainCardItem label="Validation Code" value="VALID" />
+                  <BlockchainCardItem
+                    label="Payload Hash"
+                    value="82a3665a8504337c2af00d"
+                  />
+                  <BlockchainCardItem label="Creator MSP" value="shaivMSP" />
+                  <BlockchainCardItem
+                    label="Endoser"
+                    value="{'sdfasdf', 'asdf'}"
+                  />
+                  <BlockchainCardItem label="Chaincode" value="SHaIV" />
+                  <BlockchainCardItem
+                    label="Type"
+                    value="ENDORSER_TRANSACTION"
+                  />
+                  <BlockchainCardItem
+                    label="Time"
+                    value="2020-09-02T13:36:25.469Z"
+                  />
+                </div>
+              </Card>
+            </div>
+
+            <div style={{ margin: '20px auto' }}>
+              <AccordianCard title="Reads" />
+            </div>
+
+            <div style={{ margin: '20px auto' }}>
+              <AccordianCard title="Writes" />
+            </div>
           </div>
         </div>
       </Card>
