7468a642a5b4998ff5a938cb057b99d52eac87fe
shaivpidadi <shaivpidadi@gmail.com>
2020-09-07 23:33:35 +0530

    setup accordian card

---
+++ app/components/AccordianCard/index.js
@@ -0,0 +1,17 @@
+/**
+ *
+ * AccordianCard
+ *
+ */
+
+import React from 'react';
+// import PropTypes from 'prop-types';
+// import styled from 'styled-components';
+
+function AccordianCard() {
+  return <div />;
+}
+
+AccordianCard.propTypes = {};
+
+export default AccordianCard;
