6430359ce41894cd507994ec34b1ab329cf8b898
shaivpidadi <shaivpidadi@gmail.com>
2020-09-09 20:24:00 +0530

    updated BlockchainCardItem component

--- app/components/BlockchainCardItem/index.js
+++ app/components/BlockchainCardItem/index.js
@@ -5,13 +5,28 @@
  */
 
 import React from 'react';
-// import PropTypes from 'prop-types';
-// import styled from 'styled-components';
+import PropTypes from 'prop-types';
 
-function BlockchainCardItem() {
-  return <div />;
-}
+const BlockchainCardItem = ({ label, value }) => {
+  return (
+    <div>
+      <span
+        style={{
+          color: 'rgb(93, 106, 133)',
+          width: '130px',
+          display: 'inline-block',
+        }}
+      >
+        {`${label}:`}
+      </span>
+      <span style={{ color: 'rgb(0, 0, 0)' }}>{value}</span>
+    </div>
+  );
+};
 
-BlockchainCardItem.propTypes = {};
+BlockchainCardItem.propTypes = {
+  label: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+};
 
 export default BlockchainCardItem;
