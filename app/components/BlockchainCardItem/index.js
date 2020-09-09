03a39ff6bb4ac3a2571c2cd6cbdbf42302c1a400
shaivpidadi <shaivpidadi@gmail.com>
2020-09-09 20:20:23 +0530

    BlockchainCardItem

---
+++ app/components/BlockchainCardItem/index.js
@@ -0,0 +1,17 @@
+/**
+ *
+ * BlockchainCardItem
+ *
+ */
+
+import React from 'react';
+// import PropTypes from 'prop-types';
+// import styled from 'styled-components';
+
+function BlockchainCardItem() {
+  return <div />;
+}
+
+BlockchainCardItem.propTypes = {};
+
+export default BlockchainCardItem;
