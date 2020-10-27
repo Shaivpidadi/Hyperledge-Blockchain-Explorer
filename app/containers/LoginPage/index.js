be24d981564ff11ca9926de27fbddf3f38d737a9
shaivpidadi <shaivpidadi@gmail.com>
2020-10-27 11:24:17 +0530

    Added loginForm

--- app/containers/LoginPage/index.js
+++ app/containers/LoginPage/index.js
@@ -1,7 +1,32 @@
-import React from 'react';;
+import React, { useState } from 'react';
+import { Button, FormLayout, TextField } from '@shopify/polaris';
 
 const LoginPage = () => {
-  return <div> LoginPage </div>;
-}
+  const [values, setValues] = useState({ username: '', password: '' });
+
+  console.log(values);
+  return (
+    <div class="container">
+      <h1>Login</h1>
+
+      <FormLayout>
+        <TextField
+          name="username"
+          label="username"
+          value={values?.username}
+          onChange={value => setValues({ ...values, username: value })}
+        />
+        <TextField
+          name="password"
+          type="password"
+          label="Password"
+          value={values?.password}
+          onChange={value => setValues({ ...values, password: value })}
+        />
+        <Button onClick={() => console.log(values)}>Login</Button>
+      </FormLayout>
+    </div>
+  );
+};
 
 export default LoginPage;
