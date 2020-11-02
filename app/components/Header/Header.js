9c6455092d75b9d4831b52038eca8892d8b15967
shaivpidadi <shaivpidadi@gmail.com>
2020-11-03 01:15:18 +0530

    Added home redirection

--- app/components/Header/Header.js
+++ app/components/Header/Header.js
@@ -1,19 +1,23 @@
 import React, { useState } from 'react';
 import { Avatar, TextField } from '@shopify/polaris';
+import { useHistory } from 'react-router-dom';
 
 import './Header.scss';
 const Header = () => {
   const [searchValue, setSeachValue] = useState('');
+  const history = useHistory();
   return (
     <div
       className="Header"
       style={{ display: 'flex', justifyContent: 'space-between' }}
     >
-      <Avatar
-        customer
-        name="logo"
-        source="https://banner2.cleanpng.com/20180714/rzz/kisspng-blockchain-info-cryptocurrency-bitcoin-technology-chinese-blue-5b498bcce89417.5536279015315465729527.jpg"
-      />
+      <div onClick={() => history.push('/')}>
+        <Avatar
+          customer
+          name="logo"
+          source="https://banner2.cleanpng.com/20180714/rzz/kisspng-blockchain-info-cryptocurrency-bitcoin-technology-chinese-blue-5b498bcce89417.5536279015315465729527.jpg"
+        />
+      </div>
 
       <div style={{ width: '350px' }}>
         <TextField
