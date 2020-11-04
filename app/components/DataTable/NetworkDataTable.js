bcd47898d07f0d9544509f1a820a65039490ce8b
shaivpidadi <shaivpidadi@gmail.com>
2020-11-04 11:37:10 +0530

    Added static data in NetworkDataTable

---
+++ app/components/DataTable/NetworkDataTable.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import DataTable from './DataTable';
+import TextWithEllipses from '../TextWithEllipses/TextWithEllipses';
+
+const NetworkDataTable = ({ rowsData }) => {
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: 'Peer Name',
+        accessor: 'server_hostname',
+        Cell: ({ value }) => <TextWithEllipses text={value} />
+      },
+      {
+        Header: 'Requested Url',
+        accessor: 'requests',
+        Cell: ({ value }) => <TextWithEllipses text={value} />
+      },
+      {
+        Header: 'Peer Type',
+        accessor: 'peer_type',
+      },
+      {
+        Header: 'MSPID',
+        accessor: 'mspid',
+      },
+      {
+        Header: 'High',
+        accessor: 'ledger_height_high',
+      },
+      {
+        Header: 'Low',
+        accessor: 'ledger_height_low'
+      },
+      {
+        Header: 'Unsigned',
+        accessor: 'ledger_height_unsigned',
+        Cell: ({ value }) => value.toString()
+      }
+    ],
+    [],
+  );
+
+  return (
+    <DataTable
+      rowsData={rowsData?.peers}
+      columns={columns}
+      onRowClick={(data) => console.log(data)}
+      hideFilters
+    />
+  )
+}
+
+export default NetworkDataTable;
--- app/containers/NetworkPage/index.js
+++ app/containers/NetworkPage/index.js
@@ -1,35 +1,16 @@
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useEffect, useMemo } from 'react';
 import { Card } from '@shopify/polaris';
-import { useHistory } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 
 import { getTransactionByOrgRequest, getBlockAndTransactionsListRequest } from '../../store/actions';
-import BlockDataTable from '../../components/DataTable/BlockDataTable';
+import NetworkDataTable from '../../components/DataTable/NetworkDataTable';
+
+import peerDetails from '../../../mock-data/peer-status.json';
 
 const NetworkPage = () => {
-
-  const [queryString, setQueryString] = useState({ date: '', orgs: '' });
-  const history = useHistory();
   const dispatch = useDispatch();
   const { txsByOrg, blockTxsList } = useSelector(state => state.transaction);
 
-  const onDateChange = (dates) => {
-    setQueryString({ ...queryString, date: dates });
-    let query = dates;
-    if (!!queryString.orgs) {
-      query += `&&${queryString.orgs}`
-    };
-    dispatch(getBlockAndTransactionsListRequest(query));
-  };
-
-  const onSelectChange = (selected) => {
-    setQueryString({ ...queryString, orgs: selected });
-    let query = selected;
-    if (!!queryString.date) {
-      query += `&&${queryString.date}`
-    };
-    dispatch(getBlockAndTransactionsListRequest(query));
-  }
   useEffect(() => {
     if (!txsByOrg.length) {
       dispatch(getTransactionByOrgRequest());
@@ -40,23 +21,6 @@ const NetworkPage = () => {
     dispatch(getBlockAndTransactionsListRequest());
   }, [])
 
-  const options = useMemo(() => {
-    return txsByOrg?.map((item) => ({
-      ...item,
-      value: item.creator_msp_id,
-      label: item.creator_msp_id
-    }))
-  }, [txsByOrg]) || [];
-
-  const onBlockClick = ({ blocknum }) => {
-    history.push(`/block/${blocknum}`)
-  };
-
-  const onResetClick = () => {
-    setQueryString({ date: '', orgs: '' });
-    dispatch(getBlockAndTransactionsListRequest());
-  };
-
   const blockTxsListData = useMemo(() => blockTxsList, [blockTxsList]);
 
   return (
@@ -65,14 +29,7 @@ const NetworkPage = () => {
         <Card.Header title="Network" />
 
         <div style={{ marginTop: '25px' }}>
-          <BlockDataTable
-            rowsData={blockTxsListData}
-            onBlockClick={onBlockClick}
-            onDateChange={(dates) => onDateChange(dates)}
-            dropdownOptions={options}
-            onSelectChange={onSelectChange}
-            onResetClick={onResetClick}
-          />
+          <NetworkDataTable rowsData={peerDetails} />
         </div>
       </Card>
     </div>
