04f07fb14be07ed3f7099ce05473d6f8a31bdb0b
shaivpidadi <shaivpidadi@gmail.com>
2020-11-04 15:09:08 +0530

    Added dynamic channelList

---
+++ app/components/DataTable/ChannelDataTable.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import DataTable from './DataTable';
+import TextWithEllipses from '../TextWithEllipses/TextWithEllipses';
+
+const ChannelDataTable = ({ rowsData }) => {
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: 'ID',
+        accessor: 'id',
+        Cell: ({ value }) => <TextWithEllipses text={value} />
+      },
+      {
+        Header: 'Channel Name',
+        accessor: 'channelname',
+      },
+      {
+        Header: 'Blocks',
+        accessor: 'blocks',
+      },
+      {
+        Header: 'Transactions',
+        accessor: 'transactions',
+      },
+      {
+        Header: 'Timestamp',
+        accessor: 'createdat',
+        Cell: ({ value }) => <TextWithEllipses text={value} />
+      }
+    ],
+    [],
+  );
+
+  return (
+    <DataTable
+      rowsData={rowsData}
+      columns={columns}
+      onRowClick={(data) => console.log(data)}
+      hideFilters
+    />
+  )
+}
+
+export default ChannelDataTable;
--- app/containers/ChannelPage/index.js
+++ app/containers/ChannelPage/index.js
@@ -1,5 +1,33 @@
-import React from 'react';
+import React, { useEffect, useMemo } from 'react';
+import { Card } from '@shopify/polaris';
+import { useSelector, useDispatch } from 'react-redux';
 
-const ChannelPage = () => <h1> Channels </h1>;
+import ChannelDataTable from '../../components/DataTable/ChannelDataTable';
+import { getNetworkListRequest } from '../../store/actions';
+
+const ChannelPage = () => {
+  const dispatch = useDispatch();
+  const { channelList } = useSelector(state => state.channel);
+
+  useEffect(() => {
+    if (!channelList.length) {
+      dispatch(getNetworkListRequest());
+    }
+  }, [])
+
+  const channelListData = useMemo(() => channelList, [channelList]);
+
+  return (
+    <div style={{ marginTop: '40px', width: '100%' }}>
+      <Card sectioned>
+        <Card.Header title="Network" />
+
+        <div style={{ marginTop: '25px' }}>
+          <ChannelDataTable rowsData={channelListData} />
+        </div>
+      </Card>
+    </div>
+  );
+}
 
 export default ChannelPage;
