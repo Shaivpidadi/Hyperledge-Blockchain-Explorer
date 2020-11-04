09be0fd22a5a63e8e31e50ae77ff5c487458ff6f
shaivpidadi <shaivpidadi@gmail.com>
2020-11-04 10:53:53 +0530

    Setup NetworkPage data

--- app/containers/NetworkPage/index.js
+++ app/containers/NetworkPage/index.js
@@ -1,7 +1,83 @@
-import React from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
+import { Card } from '@shopify/polaris';
+import { useHistory } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { getTransactionByOrgRequest, getBlockAndTransactionsListRequest } from '../../store/actions';
+import BlockDataTable from '../../components/DataTable/BlockDataTable';
 
 const NetworkPage = () => {
-  return <div>Network</div>;
+
+  const [queryString, setQueryString] = useState({ date: '', orgs: '' });
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const { txsByOrg, blockTxsList } = useSelector(state => state.transaction);
+
+  const onDateChange = (dates) => {
+    setQueryString({ ...queryString, date: dates });
+    let query = dates;
+    if (!!queryString.orgs) {
+      query += `&&${queryString.orgs}`
+    };
+    dispatch(getBlockAndTransactionsListRequest(query));
+  };
+
+  const onSelectChange = (selected) => {
+    setQueryString({ ...queryString, orgs: selected });
+    let query = selected;
+    if (!!queryString.date) {
+      query += `&&${queryString.date}`
+    };
+    dispatch(getBlockAndTransactionsListRequest(query));
+  }
+  useEffect(() => {
+    if (!txsByOrg.length) {
+      dispatch(getTransactionByOrgRequest());
+    }
+  }, [txsByOrg]);
+
+  useEffect(() => {
+    dispatch(getBlockAndTransactionsListRequest());
+  }, [])
+
+  const options = useMemo(() => {
+    return txsByOrg?.map((item) => ({
+      ...item,
+      value: item.creator_msp_id,
+      label: item.creator_msp_id
+    }))
+  }, [txsByOrg]) || [];
+
+  const onBlockClick = ({ blocknum }) => {
+    history.push(`/block/${blocknum}`)
+  };
+
+  const onResetClick = () => {
+    setQueryString({ date: '', orgs: '' });
+    dispatch(getBlockAndTransactionsListRequest());
+  };
+
+  const blockTxsListData = useMemo(() => blockTxsList, [blockTxsList]);
+
+  return (
+    <div style={{ marginTop: '40px', width: '100%' }}>
+      <Card sectioned>
+        <Card.Header title="Network" />
+
+        <div style={{ marginTop: '25px' }}>
+          <BlockDataTable
+            rowsData={blockTxsListData}
+            onBlockClick={onBlockClick}
+            onDateChange={(dates) => onDateChange(dates)}
+            dropdownOptions={options}
+            onSelectChange={onSelectChange}
+            onResetClick={onResetClick}
+          />
+        </div>
+      </Card>
+    </div>
+  );
+
 }
 
 export default NetworkPage;
