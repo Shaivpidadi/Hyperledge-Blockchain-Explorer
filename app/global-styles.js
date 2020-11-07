abadc6c04bf83dcd15de76fa99511aa80ffef43e
shaivpidadi <shaivpidadi@gmail.com>
2020-11-07 17:02:29 +0530

    Added scrollbar

--- app/global-styles.js
+++ app/global-styles.js
@@ -36,6 +36,39 @@ const GlobalStyle = createGlobalStyle`
   .Polaris-Heading {
     font-size: 3rem;
   }
+
+  /* Customize website's scrollbar like Mac OS
+Not supports in Firefox and IE */
+
+/* total width */
+::-webkit-scrollbar {
+    background-color:#fff;
+    width:16px
+}
+
+/* background of the scrollbar except button */
+::-webkit-scrollbar-track {
+    background-color:#fff !important;
+}
+::-webkit-scrollbar-track:hover {
+    background-color:#f4f4f4 !important;
+}
+
+/* scrollbar itself */
+::-webkit-scrollbar-thumb {
+    background-color:#babac0 !important;
+    border-radius:16px !important;
+    border:5px solid #fff !important;
+}
+::-webkit-scrollbar-thumb:hover {
+    background-color:#a0a0a5 !important;
+    border:4px solid #f4f4f4 !important;
+}
+
+/* set button(top and bottom of the scrollbar) */
+::-webkit-scrollbar-button {
+  display:none !important
+}
 `;
 
 export default GlobalStyle;
